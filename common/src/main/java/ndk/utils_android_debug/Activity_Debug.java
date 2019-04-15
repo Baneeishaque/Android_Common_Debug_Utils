663fb3a914cf5a6d217296d0d7ca81dae4016fc7
Banee Ishaque K <baneeishaque@gmail.com>
2019-04-15 23:56:35 +0530

    Initial commit

---
+++ common/src/main/java/ndk/utils_android_debug/Activity_Debug.java
@@ -0,0 +1,27 @@
+package ndk.utils_android_debug;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import tech.linjiang.pandora.Pandora;
+
+public class Activity_Debug extends AppCompatActivity {
+
+    Button button_pandora;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debug);
+
+        button_pandora = findViewById(R.id.button_pandora);
+        button_pandora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Pandora.get().open();
+            }
+        });
+    }
+}
